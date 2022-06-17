fd1364da32bee2a84a55515739102765fb3caf06
art <gulturyan.a@gmail.com>
2022-06-17 15:25:38 +0300

    add tasks for home

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let num = 50;
+//let num = 50;
 
 /*
 while (num <= 55) {
@@ -33,15 +33,63 @@ for (let i = 1; i < lenght; i++) {
     }
     result += '\n';
 }
-console.log(result);*/
-
+console.log(result);*
+/*
 first: for (let i = 0; i < 3; i++){
     console.log(`First Level: ${i}`);
     for (let j = 0; j < 3; j++){
         console.log(`Second Level: ${j}`);
         for (let k = 0; k < 3; k++){
-            if (k === 2) continue /*break*/ first;
+            if (k === 2) continue /*break* first;
             console.log(`Third Level: ${k}`);
         }
     }
 }
+//------------------
+for (let i = 5; i < 11; i++){
+    console.log(i);
+}
+
+for (let i = 20; i > 9; i--){
+    if (i === 13) {break}
+    console.log(i);
+}
+
+for (let i = 2; i < 11; i++){
+    console.log(i);
+    i +=1;
+}
+
+let i = 20;
+while (i < 21){
+    if (i===12){break;}
+    console.log(i);
+    i--;
+}
+//------------------
+
+let j = 3;
+while (j < 16){
+    if (j === 16){break;}
+    console.log(j);
+    j+=2;
+}*/
+/*
+const arrayOfNumbers = [];
+let j = 5;
+for (let i = 0; i <= 5; i++){
+        arrayOfNumbers[i] = j;
+        j++;
+}
+console.log(arrayOfNumbers);
+*/
+
+
+
+    const arrayOfNumbers = [];
+    let j = 0;
+    for (let i = 5; i <= 10; i++){
+            arrayOfNumbers[j] = i;j+=1;
+    }
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
