5be2a8b72e855f03b6e23f38c946f5331a9a2317
art <gulturyan.a@gmail.com>
2022-06-17 16:11:14 +0300

    add tasks for home

--- script.js
+++ script.js
@@ -82,14 +82,62 @@ for (let i = 0; i <= 5; i++){
         j++;
 }
 console.log(arrayOfNumbers);
-*/
-
-
-
+*
     const arrayOfNumbers = [];
-    let j = 0;
+
     for (let i = 5; i <= 10; i++){
-            arrayOfNumbers[j] = i;j+=1;
+            arrayOfNumbers[i - 5] = i;
     }
     console.log(arrayOfNumbers);
     return arrayOfNumbers;
+*/
+
+
+// Продвинутые задания на использование циклов
+
+// Место для первой задачи
+
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    // Пишем решение вот тут
+    for (let i = 0; i < arr.length; i++){
+        result[i] = arr[i];
+    }
+    //console.log(arr.length);
+    console.log(result);
+    // Не трогаем
+    //return result;
+
+// Место для второй задачи
+
+        // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+        const data = [5, 10, 'Shopping', 20, 'Homework'];
+    
+        // Пишем решение вот тут
+        for (let i = 0; i < data.length; i++){
+            
+            if (typeof(data[i])==='string'){
+                data[i] = `${data[i]} - done`;
+            } else
+            if (typeof(data[i])==='number'){
+                data[i] = data[i] * 2;
+            }
+
+        }
+        console.log(data);
+
+    //------------------------
+// Место для третьей задачи
+
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (let i=1; i <= data.length; i++){
+        result[i - 1] = data[data.length - i]
+    }
+    console.log(result);
+    // Не трогаем
+    
