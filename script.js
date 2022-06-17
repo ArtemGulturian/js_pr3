1a62a887c1fc53878c3350b41f3762b5b5ed1fd4
art <gulturyan.a@gmail.com>
2022-06-17 17:59:20 +0300

    add Christmas tree*

--- script.js
+++ script.js
@@ -149,10 +149,10 @@ let result = '';
 // Проверяется именно переменная result, формируйте строку в ней
 for (let i = 0; i <= lines; i++) {
     for (let j = 0; j < lines - i; j++){
-        result += " ";
+        result += "  ";
     }
     for (let j = 0; j < 2 * i + 1; j++){
-        result += "*";
+        result += " *";
         }
 result += '\n';
 }
