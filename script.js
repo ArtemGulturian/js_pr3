b737ba815b9d3c1f88ff3e3489ba8a8a0fe5119f
art <gulturyan.a@gmail.com>
2022-06-17 14:11:55 +0300

    add ***

--- script.js
+++ script.js
@@ -2,7 +2,35 @@
 
 let num = 50;
 
-while (num < 55) {
+/*
+while (num <= 55) {
+    console.log(num);
+    num++;
+}*
+
+do {
     console.log(num);
     num++;
 }
+while (num < 55);*
+
+for (let i = 1; i < 10;  i++) {
+    if(i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+}*/
+
+//--------------------
+
+let result = '';
+const lenght = 9;
+
+for (let i = 1; i < lenght; i++) {
+    for (let j = 0; j <i; j++){
+        result += "*";
+    }
+    result += '\n';
+}
+console.log(result);
